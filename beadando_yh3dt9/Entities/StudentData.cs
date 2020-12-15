eb31a2bb3d411ced6e520f08c48ea7977ebfd6a8
Dávid Benedek Csontos <david.csontos2@stud.uni-corvinus.hu>
2020-12-15 18:29:58 +0100

    StudentData osztály létrehozása commit

---
+++ beadando_yh3dt9/Entities/StudentData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beadando_yh3dt9.Entities
+{
+    class StudentData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Gift { get; set; }
+
+    }
+}
