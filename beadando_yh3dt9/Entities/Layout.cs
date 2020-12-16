4ff183b075ec6109e614a4bdf96f2235a34f6f76
Dávid Benedek Csontos <david.csontos2@stud.uni-corvinus.hu>
2020-12-16 01:34:35 +0100

    Fő kezelőszervek elhelyezése commit

---
+++ beadando_yh3dt9/Entities/Layout.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using beadando_yh3dt9.Entities;
+
+namespace beadando_yh3dt9.Entities
+{
+    class Layout
+    {
+        
+        Label resztvevok = new Label()
+        {
+            Width = 250,
+            Height = 40,
+            Location = new Point(20,20),
+            Text = "Résztvevő tanulók:",
+
+        };
+       
+
+        TextBox kereso = new TextBox()
+        {
+            Width = 600,
+            Height = 40,
+            Location = new Point (350,20)
+            
+        };
+
+        Button kereses = new Button()
+        {
+            Width = 300,
+            Height = 40,
+            Location = new Point(1000, 20),
+            Text = "Keresés",
+        };
+
+        Button betoltes = new Button()
+        {
+            Width = 300,
+            Height = 40,
+            Location = new Point(1500, 20),
+            Text = "Adatok betöltése",
+        };
+
+        Button sorsolas = new Button()
+        {
+            Width = 300,
+            Height = 40,
+            Location = new Point(1500, 80),
+            Text = "Sorsolás",
+        };
+
+        Button exportalas = new Button()
+        {
+            Width = 300,
+            Height = 40,
+            Location = new Point(1500, 140),
+            Text = "Adatok exportálása",
+        };
+
+        ListBox tanulok = new ListBox()
+        {
+            Width = 400,
+            Height = 1000,
+            Location = new Point (20,80)
+        };
+
+
+    }
+}
--- beadando_yh3dt9/Form1.cs
+++ beadando_yh3dt9/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using beadando_yh3dt9.Entities;
 
 
@@ -14,11 +16,82 @@ namespace beadando_yh3dt9
 {
     public partial class Form1 : Form
     {
+        BindingList<StudentData> Students = new BindingList<StudentData>();
         
+
         public Form1()
         {
             InitializeComponent();
+            void Layout()
+            {
+                Label resztvevok = new Label()
+                {
+                    Width = 100,
+                    Height = 40,
+                    Location = new Point(20, 20),
+                    Text = "Résztvevő tanulók:",
+
+                };
+                this.Controls.Add(resztvevok);
+
+                TextBox kereso = new TextBox()
+                {
+                    Width = 300,
+                    Height = 40,
+                    Location = new Point(140, 20)
+
+                };
+                this.Controls.Add(kereso);
+
+                Button kereses = new Button()
+                {
+                    Width = 300,
+                    Height = 40,
+                    Location = new Point(460, 20),
+                    Text = "Keresés",
+                };
+                this.Controls.Add(kereses);
+
+                Button betoltes = new Button()
+                {
+                    Width = 300,
+                    Height = 40,
+                    Location = new Point(780, 20),
+                    Text = "Adatok betöltése",
+                };
+                this.Controls.Add(betoltes);
+
+                Button sorsolas = new Button()
+                {
+                    Width = 300,
+                    Height = 40,
+                    Location = new Point(780, 80),
+                    Text = "Sorsolás",
+                };
+                this.Controls.Add(sorsolas);
+
+                Button exportalas = new Button()
+                {
+                    Width = 300,
+                    Height = 40,
+                    Location = new Point(780, 140),
+                    Text = "Adatok exportálása",
+                };
+                this.Controls.Add(exportalas);
+
+                ListBox tanulok = new ListBox()
+                {
+                    Width = 100,
+                    Height = 500,
+                    Location = new Point(20, 80)
+                };
+                this.Controls.Add(tanulok);
+            }
+            Layout();
+
 
         }
+
+      
     }
 }
